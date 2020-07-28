b23d6ac8017d04e12a5a6972400b0d5439c0cb23
Brandon Gart <brandongart@gmail.com>
2020-07-28 13:42:13 -0600

    added offlne feature

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -1,25 +1,78 @@
 let transactions = [];
+let unposted = [];
 let myChart;
 
-fetch("/api/transaction")
-	.then(response => {
-		return response.json();
-	})
-	.then(data => {
-		// save db data on global variable
-		transactions = data;
+if (localStorage.unposted) {
+	unposted = JSON.parse(localStorage.unposted)
+}
 
-		populateTotal();
-		populateTable();
-		populateChart();
-	});
+function saveRecord(record) {
+	unposted[unposted.length] = record;
+	localStorage.unposted = JSON.stringify(unposted);
+	
+}
+
+function wait(ms) {
+	return new Promise((resolve, reject) => { setTimeout( ()=>{resolve(); }, ms); });
+}
+/** directly async version of 'wait' */
+async function pause(ms) { await wait(ms); }
+
+async function retryFailedTransactions() {
+	while (true) {
+		
+		try {
+			if (unposted.length > 0) {
+				const response = await fetch("/api/transaction", {
+					method: "post",
+					body: JSON.stringify(unposted[0]),
+					headers: {
+						Accept: "application/json, text/plain, */*", 
+						"Content-Type": "application/json"
+					}
+				})
+				const data = await response.json();
+				
+				if (!data.errors) {
+					unposted.shift();
+					localStorage.unposted = JSON.stringify(unposted);
+					console.log("successfully sent transaction")
+				}
+			} else {
+				console.log("No Failed Transactions to send, waiting 1 minute.");
+				await pause(60000);
+			}
+		} catch (err) {
+			console.log(err);
+			console.log("Failed to resend Transaction, waiting 5 seconds.");
+			await pause(5000)
+		}
+	}
+}
+
+retryFailedTransactions();
+
+fetch("/api/transaction")
+.then(response => {
+	return response.json();
+})
+.then(data => {
+	// save db data on global variable
+	unposted.reverse();
+	transactions = [...unposted, ...data]
+	unposted.reverse();
+	
+	populateTotal();
+	populateTable();
+	populateChart();
+});
 
 function populateTotal() {
 	// reduce transaction amounts to a single total value
 	let total = transactions.reduce((total, t) => {
 		return total + parseInt(t.value);
 	}, 0);
-
+	
 	let totalEl = document.querySelector("#total");
 	totalEl.textContent = total;
 }
@@ -27,15 +80,15 @@ function populateTotal() {
 function populateTable() {
 	let tbody = document.querySelector("#tbody");
 	tbody.innerHTML = "";
-
+	
 	transactions.forEach(transaction => {
 		// create and populate a table row
 		let tr = document.createElement("tr");
 		tr.innerHTML = `
-			<td>${transaction.name}</td>
-			<td>${transaction.value}</td>
+		<td>${transaction.name}</td>
+		<td>${transaction.value}</td>
 		`;
-
+		
 		tbody.appendChild(tr);
 	});
 }
@@ -44,36 +97,36 @@ function populateChart() {
 	// copy array and reverse it
 	let reversed = transactions.slice().reverse();
 	let sum = 0;
-
+	
 	// create date labels for chart
 	let labels = reversed.map(t => {
 		let date = new Date(t.date);
 		return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
 	});
-
+	
 	// create incremental values for chart
 	let data = reversed.map(t => {
 		sum += parseInt(t.value);
 		return sum;
 	});
-
+	
 	// remove old chart if it exists
 	if (myChart) {
 		myChart.destroy();
 	}
-
+	
 	let ctx = document.getElementById("myChart").getContext("2d");
-
+	
 	myChart = new Chart(ctx, {
 		type: 'line',
-			data: {
-				labels,
-				datasets: [{
-						label: "Total Over Time",
-						fill: true,
-						backgroundColor: "#6666ff",
-						data
-				}]
+		data: {
+			labels,
+			datasets: [{
+				label: "Total Over Time",
+				fill: true,
+				backgroundColor: "#6666ff",
+				data
+			}]
 		}
 	});
 }
@@ -82,7 +135,7 @@ function sendTransaction(isAdding) {
 	let nameEl = document.querySelector("#t-name");
 	let amountEl = document.querySelector("#t-amount");
 	let errorEl = document.querySelector(".form .error");
-
+	
 	// validate form
 	if (nameEl.value === "" || amountEl.value === "") {
 		errorEl.textContent = "Missing Information";
@@ -91,22 +144,22 @@ function sendTransaction(isAdding) {
 	else {
 		errorEl.textContent = "";
 	}
-
+	
 	// create record
 	let transaction = {
 		name: nameEl.value,
 		value: amountEl.value,
 		date: new Date().toISOString()
 	};
-
+	
 	// if subtracting funds, convert amount to negative number
 	if (!isAdding) {
 		transaction.value *= -1;
 	}
-
+	
 	// add to beginning of current array of data
 	transactions.unshift(transaction);
-
+	
 	// re-run logic to populate ui with new record
 	populateChart();
 	populateTable();
@@ -126,7 +179,7 @@ function sendTransaction(isAdding) {
 	})
 	.then(data => {
 		if (data.errors) {
-			errorEl.textContent = "Missing Information";
+			errorEl.textContent = "Server Error";
 		}
 		else {
 			// clear form
@@ -137,7 +190,7 @@ function sendTransaction(isAdding) {
 	.catch(err => {
 		// fetch failed, so save in indexed db
 		saveRecord(transaction);
-
+		
 		// clear form
 		nameEl.value = "";
 		amountEl.value = "";
